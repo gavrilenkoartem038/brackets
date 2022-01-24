41edcb7bbeddde4dfddd29214c24b05455a96cc6
gavrilenkoartem038 <gavrilenkoartem038@gmail.com>
2022-01-24 18:45:41 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let startArr = str.split('')
+  let arr = []
+  for (let i = 0; i < startArr.length - 1; i++) {
+    arr = [startArr[i], startArr[i+1]]
+    if (bracketsConfig.some(elem => elem.every((e, index) => e == arr[index]))) {
+      console.log(startArr)
+      startArr.splice(i, 2)
+      i = i - 2
+    }
+  }
+  return startArr.length == 0
 }
